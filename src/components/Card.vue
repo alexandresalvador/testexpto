7c49b70fdc7cff42d9b8b94659c413c2b4489cfd
aluno-node <aluno-node@c678d.bonja.local>
2022-03-30 19:18:10 -0300

    primeiro commit

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <h1>Card Componente</h1>
+    <!-- <p>{{name}}</p>
+<p>{{description}}</p> -->
+    <p>{{ person }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardProducts',
+  props: {
+    person: {
+      type: Object,
+      // required: true
+    },
+  },
+  data() {
+    return {
+      //  name: 'Notebook',
+      //   description: 'Core5, 2.6ghz, 8gb, tela whitescreen 15,4',
+    };
+  },
+};
+</script>
+<style scoped>
+.card {
+  max-width: 400px;
+  max-height: 200px;
+  background-color: #eee;
+  border: 1px solid #999;
+  color: black;
+  padding: 10px;
+}
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Person</h1>
+    <label for="name"
+      >Nome| {{ person }} <br />
+      <input type="text" v-model="name" placeholder="Insira seu nome" />
+    </label>
+
+    <hr />
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'Person',
+  data() {
+    return {
+      name: '',
+      email: '',
+      age: '',
+      bio: '',
+      isVisible: false,
+    };
+  },
+};
+</script>
